b6c994430c6f26fe0ecf2a1a8c42cbc677f24233
Luis Alberto Fernandez Calleja <alberto.fernandez@marfeel.com>
2021-03-28 14:18:36 +0200

    reverting unnecesary changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TodoList from './features/todo/TodoList'
 import AddTodoForm from './features/todo/AddTodoForm'
 import './App.css';
-class App extends Component {
 
-  render(){
-    return (
-      <div className="App">
-        <AddTodoForm />
-        <TodoList />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <AddTodoForm />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
