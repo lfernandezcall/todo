e0ef9dcb121fc0d83568d49a75e70823f698e4ef
Luis Alberto Fernandez Calleja <alberto.fernandez@marfeel.com>
2021-03-29 01:33:34 +0200

    able to modify text on existing todos

--- src/features/todo/TodoList.js
+++ src/features/todo/TodoList.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Context } from '../../app/StateHandler'
 import ModifyTodo from './ModifyTodo'
 
 const TodoList = () => {
-    const { state } = Context();
+    const { state, dispatch } = Context();
+    const inputRef = useRef();
+    const saveEdit = (e, item) => {
+        e.preventDefault()
+        dispatch({ id: item.id, type: 'editItem', editText: inputRef.current.value })
+    }
 
     return (
-        <ul>{state.map(item => (
-            item.modified === true ?
-            (<p key={'modify'}>Wants to modify???</p>)
+        <ul>{state.map(item => (item.modified === true ?
+            (<form key={item.id} onSubmit={(e) => saveEdit(e, item)}>
+                <input
+                    key={item.id}
+                    ref={inputRef}
+                    type='text'
+                    placeholder={item.text}
+                />
+            </form>)
             :
-            (<div>
+            (<div key={item.id}>
                 <li key={item.id} className={item.done === true ? 'completed' : ''}>{item.text}</li>
                 <ModifyTodo id={item.id} />
             </div>)
